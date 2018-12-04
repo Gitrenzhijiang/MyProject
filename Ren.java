47fbfb08f386e74bba2d922c6c6dd8d759d74b86
Gitrenzhijiang <40815622+Gitrenzhijiang@users.noreply.github.com>
2018-12-04 11:25:21 +0800

    Update Ren.java
    测试冲突 test conflect

--- Ren.java
+++ Ren.java
@@ -1,5 +1,9 @@
 public class Ren{
 	public static void main(String[]args){
-		// renzhijiang editor...// 2018-12-4 你好？？？， 我的提交
+		// renzhijiang editor...// 2018-12-4 你好？？？， 我的提交 joiw new commit by renzhijiang
+		// ookikokk
+	}
+	public static void ren(){
+		// ......
 	}
 }
